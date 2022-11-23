7de7f80c152fafbdf8ead352a85202a5d5e8e89a
kauancampos <kauanpablofreitascampos@gmail.com>
2022-11-23 19:59:53 -0300

    Atualização #41

--- src/js/login.js
+++ src/js/login.js
@@ -1,12 +1,11 @@
 var atual; //Armazena o container que está sendo apresentado na tela (cadastro ou relatório)
 var localStorage = Window.localStorage;
 
-function buscaUsuario(nome){
+function buscaUsuario(nome, senha){
     var jt = localStorage.getItem(nome); 
-    let teste = document.getElementById("senha").value;
     if(jt!=null){ 
         var pessoa = JSON.parse(jt); 
-        if (document.getElementById("nome").value === pessoa.nome && document.getElementById("senha").value === pessoa.senha) {
+        if (document.getElementById("nome").value === pessoa.nome && senha === pessoa.senha) {
             console.log("login sucesso");
             window.location.href = "http://127.0.0.1:5502/src/pagina-iniciallogada.html";
         }
